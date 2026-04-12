f4541681433628332a08dec67191ec0abf28d27b
Hal Rogers <hal.c.rogers@gmail.com>
2026-04-12 16:16:59 -0400

    init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package com.break2bits
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(args: Array<String>) {
+    println("args: $args")
+    println("Finished!")
+}
