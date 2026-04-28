4adf0115e241bbc47f4d552c463e97483402e292
Hal Rogers <hal.c.rogers@gmail.com>
2026-04-28 11:12:00 -0400

    Implement software attr

--- src/main/kotlin/handler/StunHandler.kt
+++ src/main/kotlin/handler/StunHandler.kt
@@ -19,19 +19,24 @@ class StunHandler(
     private val messageIntegrityValidator: MessageIntegrityValidator,
     private val xorMappedAddressValueSerializer: XorMappedAddressValueSerializer
 ) {
+    private companion object {
+        private const val SOFTWARE_NAME = "stun-server-kt"
+    }
+
     fun handle(message: StunMessage, senderAddress: InetAddress, senderPort: Int): StunMessage? {
         validateMessageType(message.header)
 
         // if indication, no response is necessary
         val responseMessageType = StunMessageType.BINDING_RESPONSE
 
+        // authentication is optional
+
         val hadFingerprint = maybeValidateFingerprintAttribute(message)
         val xorMappedAddressAttr = createXorMappedAddressAttribute(message.header, senderAddress, senderPort)
-        // authentication is optional
-        // messageIntegrityValidator.validate(message)
-        // check for fingerprint
-        // add xor mapped address
-        // add software attribute
+        val softwareAttr = createSoftwareAttribute()
+        if (hadFingerprint) { // if there was a fingerprint in the request we should add one to the response
+
+        }
 
         throw NotImplementedError("Message processing not yet implemented")
     }
@@ -59,6 +64,22 @@ class StunHandler(
             throw StunRequestException("${StunAttributeType.FINGERPRINT.name} attribute value ($${messageFingerprint.toHexString()}) not equal to calculated fingerprint: ${calculatedFingerprint.toHexString()}")
         }
         return true
+    }
+
+    private fun createSoftwareAttribute(): StunAttribute {
+        var softwareAttrValueBytes = SOFTWARE_NAME.encodeToByteArray()
+        val bytesToPrepend = softwareAttrValueBytes.size % 4
+        if (bytesToPrepend > 0) { // the value must end on a 32-bit boundary
+            val paddedSoftwareAttrValueBytes = ByteArray(softwareAttrValueBytes.size + bytesToPrepend)
+            System.arraycopy(softwareAttrValueBytes, 0, paddedSoftwareAttrValueBytes, bytesToPrepend, softwareAttrValueBytes.size)
+            softwareAttrValueBytes = paddedSoftwareAttrValueBytes
+        }
+        return StunAttribute(
+            type = StunAttributeType.SOFTWARE,
+            valueLength = softwareAttrValueBytes.size.toUShort(),
+            value = softwareAttrValueBytes,
+            offset = -1
+        )
     }
 
     private fun createXorMappedAddressAttribute(header: StunHeader, senderAddress: InetAddress, senderPort: Int): StunAttribute {
